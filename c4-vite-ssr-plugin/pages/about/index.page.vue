7e8e060847937360af8cfcd3ed487ffa659f9d57
ThinhVu <vutrongthinhk7@gmail.com>
2023-03-05 13:42:44 +0700

    Add c4-vite-ssr-plugin

---
+++ c4-vite-ssr-plugin/pages/about/index.page.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>About</h1>
+  <p>Demo app using <code>vite-plugin-ssr</code>.</p>
+</template>
+
+<style>
+code {
+  font-family: monospace;
+  background-color: #eaeaea;
+  padding: 3px 5px;
+  border-radius: 4px;
+}
+</style>
